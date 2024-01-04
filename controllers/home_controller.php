3a0da3683510cc0ca41d2e8c748035c2ba6aec75
alifertah <alifertah42@gmail.com>
2024-01-04 09:52:23 +0100

    feat : fetching the register traitement and handling the response

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,11 +1,1 @@
 <?php
-
-echo '<h3>- Home Controller -</h3>';
-
-if (isset($_POST['login'])) {
-    echo 'Is login';
-}
-
-if (isset($_POST['logout'])) {
-    echo 'Is logout';
-}
--- views/register_view.php
+++ views/register_view.php
@@ -20,6 +20,7 @@
 		<button
 			class="inline-block mt-2 bg-green-600 hover:bg-green-700 focus:bg-green-800 px-6 py-2 rounded text-white shadow-lg"
 			type="submit" name="register"
+			id='register'
 		>
 			Regsiter
 		</button>
@@ -41,3 +42,19 @@
 		&copy;2022 Acme Corporation. All rights reserved.
 	</p>
 </div>
+
+<script>
+	const formData = new FormData();
+	document.getElementById('register').addEventListener('click', (event)=>{
+		event.preventDefault()
+		formData.append(username, document.getElementById("username").value)
+		formData.append(email, document.getElementById("email").value)
+		formData.append(password, document.getElementById("password").value)
+		fetch("index.php?page=register_traitement")
+		.then(response => response.json())
+		.then(data =>{
+			if(data.status == 200)
+				console.log("yes")
+		})
+	})
+</script>
